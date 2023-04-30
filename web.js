8b83e312c4f2e899713792e59aa3058b78b351d2
muzhidalmoon <127823736+muzhidalmoon@users.noreply.github.com>
2023-04-30 09:22:42 +0600

    Add files via upload

---
+++ web.js
@@ -0,0 +1,12 @@
+    document.getElementsByTagName('button')[0].addEventListener('click',()=>{
+        document.getElementById('nav-links').classList.toggle('active')
+    })
+
+
+   document.getElementById("seo").addEventListener("click", () => {
+     document.getElementById("boxes").classList.toggle("run");
+     document.getElementById("seo").classList.toggle("bgBlue");
+   });
+
+ 
+    
